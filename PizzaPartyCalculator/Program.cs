97cd7eba2ad860c9ab1112eab47f210eb2a4ab0e
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2024-09-19 01:07:32 +0000

    Module2Lab

---
+++ PizzaPartyCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿//Delaring the vairbale
+int numberOfGuests;
+int slicesPerGuest =3; //Assuming each guest eats 3 slices of zaa
+int slicesPerPizza = 8; // Assuming the standard slice of pizza has 8 slices
+
+//Prompt user tp enter the # of guest
+Console.Write("Enter the number of guest: ");
+//Call the parse function to turn the value the user entered into 
+//an int
+numberOfGuests = int.Parse(Console.ReadLine());
+
+//calc the number of slices needed
+int totalSlicesNeeded = numberOfGuests * slicesPerGuest;
+
+//calc the number of pizzas reqiured
+int reqiuredPizzas =(int)Math.Ceiling((double)totalSlicesNeeded / slicesPerPizza);
+//takes our result and roounds it up to the next math interger
+
+//Outputs the result to User
+Console.WriteLine($"for {numberOfGuests}, you'll need {reqiuredPizzas} pizzas");
